5387887f8e9fa8d18113a024b928645fd0559d02
三十文 <1047210087@qq.com>
2018-05-14 15:53:27 +0800

    add backend

---
+++ src/routes/Backend/TableEdit/TableEdit.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class TableEdit extends React.Component {
+
+  constructor () {
+    super();
+  }
+
+  render () {
+
+  }
+
+}
--- src/routes/RoleCreation/RoleCreation.js
+++ src/routes/RoleCreation/RoleCreation.js
@@ -6,18 +6,27 @@ import Swiper from 'swiper/dist/js/swiper.min';
 import Typed from 'typed.js';
 import $ from 'jquery';
 
+/**
+ * 流程
+ */
 const RenderSteps = {
   renderGender: Symbol('gender'),
   renderNickname: Symbol('nickname'),
   renderFamilyCircumstances: Symbol('family'),
 };
 
+/**
+ * 性别
+ */
 const GenderTypes = {
   PRINCE: 1,
   PRINCESS: 2,
 };
 
-const FamilyCircumstances = {
+/**
+ * 家境
+ */
+const Family = {
   AAAA: 1,
   BBBB: 2,
   CCCC: 3,
@@ -34,6 +43,7 @@ export default class RoleCreation extends React.Component {
       gender: GenderTypes.PRINCE,
       // 昵称
       nickname: '无名浪人',
+      family: Family.AAAA,
     };
   }
 
@@ -46,38 +56,77 @@ export default class RoleCreation extends React.Component {
     //   strings: ['i want be a hero', 'how about you'],
     //   typeSpeed: 40,
     // });
+    $('#box').animateCss('animated fadeInRight');
   }
 
+  handleClickChooseGender (gender) {
+    this.setState({
+      gender: gender,
+    }, () => {
+      this.nextStep();
+    });
+  }
+
+  handleClickInsertNickname (nickname) {
+    this.setState({
+      nickname: nickname,
+    });
+  }
+
+  handleClickChooseFamily (family) {
+    this.setState({ family: family }, () => {
+      this.nextStep();
+    });
+  }
+
+  /**
+   * 性别页面渲染
+   */
   renderGender () {
     return (
       <div>
-        <h2>性别选择提示语</h2>
-        <Button>王子</Button>
-        <Button>公主</Button>
+        <p>某年某日，</p>
+        <p>一个宁静的小镇，</p>
+        <p>有一位</p>
+        <h2>选择您的性别</h2>
+        <Button onClick={() => this.handleClickChooseGender(GenderTypes.PRINCE)}>王子</Button>
+        <Button onClick={() => this.handleClickChooseGender(GenderTypes.PRINCESS)}>公主</Button>
+        <p>诞生了。</p>
       </div>
     );
   }
 
+  /**
+   * 昵称页面渲染
+   */
   renderNickname () {
     return (
       <div>
-        <h2>昵称输入提示语</h2>
+        <p>父母为他取名为</p>
+        <h2>输入您的昵称</h2>
         <List>
           <InputItem placeholder={'输入您的江湖大名'}
                      maxLength={8}
-                     onChange={(v) => console.log(v)}/>
+                     onChange={(v) => this.handleClickInsertNickname(v)}/>
         </List>
+        <br/>
+        <Button onClick={() => this.nextStep()}>确认</Button>
       </div>
     );
   }
 
+  /**
+   * 家境情况页面渲染
+   */
   renderFamilyCircumstances () {
     return (
       <div>
-        <h2>家境选择提示语</h2>
-        <Button>土豪</Button>
-        <Button>弱鸡</Button>
-        <Button>贫民</Button>
+        <p>江湖风雨从未刮到过这里，</p>
+        <p>但为这个人的未来扑下了一条布满荆棘的未知道路。</p>
+        <h2>选择您的家境</h2>
+        <Button onClick={() => this.handleClickChooseFamily(Family.AAAA)}>土豪</Button>
+        <Button onClick={() => this.handleClickChooseFamily(Family.BBBB)}>弱鸡</Button>
+        <Button onClick={() => this.handleClickChooseFamily(Family.CCCC)}>贫民</Button>
       </div>
     );
   }
@@ -95,6 +144,9 @@ export default class RoleCreation extends React.Component {
     }
   }
 
+  /**
+   * 触发下一步
+   */
   nextStep () {
     const { step } = this.state;
     let nextStep;
@@ -106,19 +158,19 @@ export default class RoleCreation extends React.Component {
         nextStep = RenderSteps.renderFamilyCircumstances;
         break;
       case RenderSteps.renderFamilyCircumstances:
-        this.props.history.push({
-          pathname: '/hello/1',
-          state: {
-            id: 1,
-          },
-        });
+        // this.props.history.push({
+        //   pathname: '/hello/1',
+        //   state: {
+        //     id: 1,
+        //   },
+        // });
+        console.log(this.state);
         break;
       default:
         break;
     }
-    this.setState({ step: nextStep }, () => {
-      $('#box').animateCss('animated fadeInRight');
-    });
+    this.setState({ step: nextStep });
+    $('#box').animateCss('animated fadeInRight');
   }
 
   render () {
@@ -136,8 +188,6 @@ export default class RoleCreation extends React.Component {
              className="content-box">
           {this.renderBySteps(step)}
         </div>
-        <FooterButton text={'hello'}
-                      onClick={() => this.nextStep()}/>
       </div>
     );
   }
