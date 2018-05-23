91a0c959beb9419bafcff5f0b6aada02e210565d
三十文 <1047210087@qq.com>
2018-05-23 15:51:41 +0800

    add iconfont

--- src/index.js
+++ src/index.js
@@ -1,41 +1,41 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store/createStore';
-import Routes from './routes/router';
-import $ from 'jquery';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store/createStore'
+import Routes from './routes/router'
+import $ from 'jquery'
 
 /**
  * 全局三方功能样式
  */
-import 'animate.css/animate.min.css';
-import 'swiper/dist/css/swiper.min.css';
-import 'antd/dist/antd.css';
+import 'animate.css/animate.min.css'
+import 'swiper/dist/css/swiper.min.css'
+import 'antd/dist/antd.css'
 /**
  * 优化 css
  */
-import 'normalize.css/normalize.css';
-import './styles/mixins.less';
+import 'normalize.css/normalize.css'
+import './styles/mixins.less'
 
 /**
  * 全局 js 操作
  */
-require('babel-polyfill');
+require('babel-polyfill')
 // 给 jquery 添加 animateCss 支持
 $.fn.extend({
   animateCss: function (animationName, callback) {
-    let animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    let animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend'
     this.addClass('animated ' + animationName).one(animationEnd, function () {
-      $(this).removeClass('animated ' + animationName);
-      if (callback) callback();
-    });
-    return this;
+      $(this).removeClass('animated ' + animationName)
+      if (callback) callback()
+    })
+    return this
   },
-});
+})
 
 ReactDOM.render(
   <Provider store={store}>
     {Routes}
   </Provider>,
   document.querySelector('#root'),
-);
+)
--- src/routes/RoadMap/RoadMap.js
+++ src/routes/RoadMap/RoadMap.js
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import './RoadMap.less'
 import { Grid } from 'antd-mobile'
 
 export default class RoadMap extends React.Component {
@@ -6,7 +7,13 @@ export default class RoadMap extends React.Component {
   constructor () {
     super()
     this.state = {
-      gridData: Array.from(new Array(100).map(() => ({ text: '' }))),
+      position: {
+        x: 0,
+        y: 0,
+      },
+      gridData: Array.from(new Array(10)).map((_val, i) => ({
+        index: i,
+      })),
     }
     this.itemStyle = {
       border: '1px solid pink',
@@ -15,14 +22,65 @@ export default class RoadMap extends React.Component {
 
   render () {
     const {
+      position,
       gridData,
     } = this.state
 
+    const renderMap = () => {
+      let gridList = []
+      for (let i = 0; i < 10; i++) {
+        gridList.push(
+          <Grid key={i}
+                data={gridData}
+                hasLine={true}
+                itemStyle={this.itemStyle}
+                columnNum={10}
+                renderItem={item => {
+                  return (
+                    <div id={`grid-item-${item.index}-${i}`}
+                         className={`${position.x === item.index && position.y === i && 'active'}`}></div>
+                  )
+                }}
+                onClick={(object, index) => {
+                  let preActiveNodes = document.querySelectorAll('.active')
+                  preActiveNodes.forEach(preActiveNode => preActiveNode.classList.remove('active'))
+                  let itemNode = document.getElementById(`grid-item-${index}-${i}`)
+                  itemNode.classList.add('active')
+                }}/>,
+        )
+      }
+      return gridList
+    }
+
     return (
       <section className="road-map-container">
-        <Grid data={gridData}
-              columnNum={10}
-              itemStyle={this.itemStyle}/>
+        <div>
+          {/*{renderMap()}*/}
+        </div>
+        <h1 onClick={() => {
+          this.setState({
+            position: { x: position.x, y: position.y - 1 },
+          })
+        }}>上 </h1>
+        <h1 onClick={() => {
+          this.setState({
+            position: { x: position.x, y: position.y + 1 },
+          })
+        }}>下 </h1>
+        <h1 onClick={() => {
+          this.setState({
+            position: { x: position.x - 1, y: position.y },
+          })
+        }}>左 </h1>
+        <h1 onClick={() => {
+          this.setState({
+            position: { x: position.x + 1, y: position.y },
+          })
+        }}>右 </h1>
+        <svg class="icon">
+          <use xlinkHref="#icon-zhushou"></use>
+        </svg>
+        <i className="iconfont icon-camera"></i>
       </section>
     )
   }
