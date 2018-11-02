d2a07e991cb42eedee12e1325ed2bb31011c3491
三十文 <1047210087@qq.com>
2018-11-02 10:50:53 +0800

    f

--- src/component/Audio/Audio.js
+++ src/component/Audio/Audio.js
@@ -9,21 +9,28 @@ export default class Audio extends React.Component {
 
   render () {
     const {
+      show = false,
       src,
       autoPlay = false,
       loop = false
     } = this.props
 
     return (
-      <audio src={src}
-             autoPlay={autoPlay}
-             loop={loop}></audio>
+      <div className="audio-component">
+        {
+          show &&
+          <audio src={src}
+                 autoPlay={autoPlay}
+                 loop={loop}></audio>
+        }
+      </div>
     )
   }
 
 }
 
 Audio.propTypes = {
+  show: propTypes.bool,       // 是否显示
   src: propTypes.string,      // 音频资源链接
   autoPlay: propTypes.bool,   // 是否自动播放
   loop: propTypes.bool,       // 是否循环播放
